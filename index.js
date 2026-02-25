837ab3095c6bafb2e897aeaac1c2b083fb7cdd0f
Akhil Mohan <akhil205.mohan@gmail.com>
2026-02-25 16:17:42 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import express from 'express'
 const app=express()
+app.set("view engine","ejs")
+const products=[
+    {id:1,name:"product 1",price:30},
+    {id:2,name:"product 2",price:40},
+    {id:3,name:"product 3",price:50}
+]
+//render html file with data to the client
 app.listen(8080,()=>
     {
         console.log("Sever started")
     })
  app.get("/",(req,res)=>
     {
-        console.log(first)
+        // res.json({message:"Hello World..!!"})
+        res.render("index",{name:"John"})
+        //returns html file as output
+
+        //html file created in the server
     })   
+
+
+//page refreshes on every change making it slower    
+app.get("/products",(req,res)=>
+    {
+        res.render("products",{products})
+    })
